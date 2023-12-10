42f42066cebcf06fde6d45647fb0447ae01d4407
jebibot <83044352+jebibot@users.noreply.github.com>
2023-12-10 20:06:22 +0900

    fix: lint, runtime

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -1,6 +1,8 @@
 import { revalidateTag } from "next/cache";
 import { NextRequest } from "next/server";
 
+export const runtime = "edge";
+
 export async function GET(request: NextRequest) {
   const tag = request.nextUrl.searchParams.get("tag");
   if (tag) {
