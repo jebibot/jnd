ced841e111db0823f1f27407c8911051e6c79aaa
jebibot <83044352+jebibot@users.noreply.github.com>
2023-12-12 06:56:17 +0900

    fix(timer): initial state

--- src/components/timer.tsx
+++ src/components/timer.tsx
@@ -4,7 +4,9 @@ import { useEffect, useState } from "react";
 import { formatTimestamp } from "@/utils/util";
 
 export default function Timer({ start }: { start: number }) {
-  const [seconds, setSeconds] = useState(0);
+  const [seconds, setSeconds] = useState(
+    Math.floor((Date.now() - start) / 1000),
+  );
 
   useEffect(() => {
     const update = () => setSeconds(Math.floor((Date.now() - start) / 1000));
