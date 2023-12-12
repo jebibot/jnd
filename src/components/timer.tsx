ea49f7ca8f48c8e4b4f7828f121bfbd0f949dee1
jebibot <83044352+jebibot@users.noreply.github.com>
2023-12-12 23:44:05 +0900

    fix: timer static SSR

--- src/components/timer.tsx
+++ src/components/timer.tsx
@@ -4,9 +4,7 @@ import { useEffect, useState } from "react";
 import { formatTimestamp } from "@/utils/util";
 
 export default function Timer({ start }: { start: number }) {
-  const [seconds, setSeconds] = useState(
-    Math.floor((Date.now() - start) / 1000),
-  );
+  const [seconds, setSeconds] = useState<number | null>(null);
 
   useEffect(() => {
     const update = () => setSeconds(Math.floor((Date.now() - start) / 1000));
@@ -15,9 +13,11 @@ export default function Timer({ start }: { start: number }) {
     return () => clearInterval(interval);
   }, [start]);
 
-  return (
+  return seconds != null ? (
     <span role="timer" suppressHydrationWarning>
       {formatTimestamp(seconds * 1000)}
     </span>
+  ) : (
+    <div className="inline-block w-12 h-2.5 rounded-full bg-gray-300 dark:bg-gray-600 animate-pulse"></div>
   );
 }
